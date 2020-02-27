79750bbb914227288d7f70fecc83c7062137967c
Paul Easby <prepre73@gmail.com>
2020-02-27 23:00:32 +0000

    save as html WIP

--- ticket-list/app/core/App.php
+++ ticket-list/app/core/App.php
@@ -3,5 +3,11 @@ class App {
 
     public function __construct () {
         echo "test";
+        return echo $this->create_html ();
+    }
+
+    public function create_html () {
+        ob_start();
+        file_put_contents('index.html', ob_get_clean());
     }
 }
